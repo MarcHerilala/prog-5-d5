cc2dc1546a594b7a55b7675c5c35f06c9eda7492
Marc985 <hei.marc.3@gmail.com>
2025-05-28 09:44:51 +0300

    add main test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+
+import modal.Coffee;
+import modal.CoffeeMachine;
+import modal.Money;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+      Coffee expresso=new Coffee("Expresso", "hot", 2.5);
+        Coffee cappuccino=new Coffee("Cappuccino", "hot", 3.0);
+        Money money=new Money(1);
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.addCoffee(expresso, 2);
+        coffeeMachine.addCoffee(cappuccino, 5);
+
+        coffeeMachine.serveCoffee(expresso,money);
     }
+
 }
